7b464971d989f69c8c70345fe69c2f923e57bd26
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-09 11:46:18 +0100

    cambio 2

--- src/com/Braiskiskos/MisMetodos.java
+++ src/com/Braiskiskos/MisMetodos.java
@@ -3,6 +3,6 @@ package com.Braiskiskos;
 public class MisMetodos {
 
     public static void saludo() {
-        System.out.println("Hola libreria CAMBIO");
+        System.out.println("Hola libreria CAMBIO erge");
     }
 }
